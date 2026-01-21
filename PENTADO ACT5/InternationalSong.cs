647ea3c8abda53ea67887f6a9702ae5bba40b66c
ACT-STUDENT <ACT-STUDENT@LAB3-PC30>
2026-01-21 08:57:43 +0800

    ACT 5

---
+++ PENTADO ACT5/InternationalSong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PENTADO_ACT5
+{
+    class InternationalSong : Song
+    {
+        public string Country { get; set; }
+
+        private InternationalSong(string title, string artist, string country)
+            : base(title, artist)
+        {
+            Country = country;
+        }
+
+        public static InternationalSong Create(string title, string artist)
+        {
+            Console.Write("Country: ");
+            string country = Console.ReadLine();
+            return new InternationalSong(title, artist, country);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("--- INTERNATIONAL SONG ---");
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Artist: " + Artist);
+            Console.WriteLine("Country: " + Country);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PENTADO ACT5/LocalSong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PENTADO_ACT5
+{
+    class LocalSong : Song
+    {
+        public string Language { get; set; }
+
+        private LocalSong(string title, string artist, string language)
+            : base(title, artist)
+        {
+            Language = language;
+        }
+
+        public static LocalSong Create(string title, string artist)
+        {
+            Console.Write("Language: ");
+            string language = Console.ReadLine();
+            return new LocalSong(title, artist, language);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("---- LOCAL SONG ----");
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Artist: " + Artist);
+            Console.WriteLine("Language: " + Language);
+            Console.WriteLine();
+        }
+    }
+}
